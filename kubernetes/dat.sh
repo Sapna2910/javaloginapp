ea154f20153f74af2afe834a1974d7bcbd8d72ae
Sapna2910 <88184248+Sapna2910@users.noreply.github.com>
2022-08-02 00:12:28 +0530

    Update dat.sh

--- kubernetes/dat.sh
+++ kubernetes/dat.sh
@@ -1,7 +1,9 @@
 if [ -z "$(helm datree version)" ]
 then
+echo "inside then"
 helm plugin install https://github.com/datreeio/helm-datree
 helm datree test myapp/
 else
+echo "inside else"
 helm datree test myapp/
 fi
